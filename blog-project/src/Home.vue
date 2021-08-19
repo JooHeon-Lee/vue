d8c1c9f0cd9a9b4525d6431cd752da6cecfb6db8
JooHeon-Lee <58197251+JooHeon-Lee@users.noreply.github.com>
2021-08-19 23:24:09 +0900

    .

---
+++ blog-project/src/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>{{ homeTitle }}</h1>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                homeTitle : "홈 입니다."
+            }
+        }
+    }
+</script>
---
+++ blog-project/src/Status.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1> status 입니다.</h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
