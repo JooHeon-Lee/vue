7c4cdc674b0b8ded2facbab094f2dbad9a50507b
JooHeon-Lee <58197251+JooHeon-Lee@users.noreply.github.com>
2021-08-21 21:29:42 +0900

    .

---
+++ miniproject/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div>
+    <h4> XX 원룸 </h4>
+    <p>XX 만원</p>
+  </div>
+  <div>
+    <h4> XX 원룸 </h4>
+    <p>XX 만원</p>
+  </div>  
+</template>
+
+<script>
+
+export default {
+  name: 'App',  
+  data() {
+    return {
+      name : "원룸 이름"
+    }
+  },
+  components: {
+
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
